15da6d852107536cb963bf6d1ba6376c18a7f513
AndrRok <rokhmanov2000@yandex.ru>
2024-06-05 21:23:57 +0400

    initial

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by ARMBP on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
