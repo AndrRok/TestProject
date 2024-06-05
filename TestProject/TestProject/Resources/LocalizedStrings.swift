d677b2fa8efd93180006717166008014464780a9
AndrRok <rokhmanov2000@yandex.ru>
2024-06-05 23:13:27 +0400

    WB-001 Первое ДЗ

---
+++ TestProject/TestProject/Resources/LocalizedStrings.swift
@@ -0,0 +1,20 @@
+//
+//  LocalizedStrings.swift
+//  TestProject
+//
+//  Created by ARMBP on 6/5/24.
+//
+
+import Foundation
+
+
+enum Strings {
+    public static let initialLogo = "Общайтесь с друзьями\nи близкими легко"
+    public static let userAgreement = "Пользовательское соглашение"
+    public static let startToHang = "Начать общаться"
+    public static let enterPhoneNumber = "Введите номер телефона"
+    public static let wellSendPinToYourNumber = "Мы вышлем код подтверждения на указаный номер"
+    public static let nextStep = "Продолжить"
+    public static let plusSevenNumber = "+7"
+    public static let phoneNumberPlaceHolder = "000 000-00-00"
+}
