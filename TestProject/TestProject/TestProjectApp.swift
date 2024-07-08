6ecedd778eb1108f9ff563cdb35fbb3574817c86
AndrRok <rokhmanov2000@yandex.ru>
2024-07-08 12:39:53 +0400

    Picker

--- TestProject/TestProject/TestProjectApp.swift
+++ TestProject/TestProject/TestProjectApp.swift
@@ -7,13 +7,33 @@
 
 import SwiftUI
 
+enum HWCases {
+    case mainWB, widget, infiniteScroll, none
+}
+
 @main
 struct TestProjectApp: App {
+    
+    @State private var appMode: HWCases = .none
+    
     var body: some Scene {
         WindowGroup {
-            //InitialScreen()
-            //ContentView()//поменять если нужен виджет
-            InfiniteScrollView()//если нужен бесконечный скролл
+            
+            switch appMode {
+                case .mainWB:
+                    InitialScreen()
+                case .widget:
+                    ContentView()
+                case .infiniteScroll:
+                    InfiniteScrollView()
+                case .none:
+                    Picker("Mode", selection: $appMode) {
+                        Text("MainWB").tag(HWCases.mainWB)
+                        Text("Widget").tag(HWCases.widget)
+                        Text("InfiniteScroll").tag(HWCases.infiniteScroll)
+                        Text("Please Choose The mode, it will be permanent until app restart").tag(HWCases.none)
+                    }
+            }
         }
     }
 }
